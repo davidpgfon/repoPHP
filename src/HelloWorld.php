ad3b7106fe06eb252afb0cbc812a243b8620dba6
Davidpgfon <david.palomero@fon.com>
2013-03-08 09:30:56 +0100

    añadida carpeta src

---
+++ src/HelloWorld.php
@@ -0,0 +1,23 @@
+<?php
+
+    /**
+     * The Hello World class!
+     *
+     */
+    class HelloWorld
+    {
+        public function foo($silent = true)
+        {
+            if ($silent) {
+                return;
+            }
+            return 'foo';
+        }
+
+        function sayHello()
+        {
+            return "Hello World!";
+        }
+    };
+
+?>
