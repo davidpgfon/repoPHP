50ddbffc20e8b7e69f77ccae07a601b947b25fe1
Davidpgfon <david.palomero@fon.com>
2013-03-22 13:44:41 +0100

    pruebas

--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -14,30 +14,46 @@
             return 'foo';
         }
 
+	public function foo2($silent = true)
+        {
+            if ($silent) {
+                return;
+            }
+            return 'foo';
+        }
+
+	public function foo21($silent = true)
+        {
+            if ($silent) {
+                return;
+            }
+            return 'foo';
+        }
+
         function sayHello()
         {
             return "Hello World!";
         }
-function sayHello2()
-        {
-            return "Hello World!";
-        }
-function sayHello3()
-        {
-            return "Hello World!";
-        }
-function sayHello4()
-        {
-            return "Hello World!";
-        }
-function sayHello5()
-        {
-            return "Hello World!";
-        }
-function sayHello6()
-        {
-            return "Hello World!";
-        }
+	function sayHello2()
+		{
+		    return "Hello World!";
+		}
+	function sayHello3()
+		{
+		    return "Hello World!";
+		}
+	function sayHello4()
+		{
+		    return "Hello World!";
+		}
+	function sayHello5()
+		{
+		    return "Hello World!";
+		}
+	function sayHello6()
+		{
+		    return "Hello World!";
+		}
 
     };
 
